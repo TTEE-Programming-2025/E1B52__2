ceaa2bc715f954e8f9b62de385fd56e6bfbf9c0b
simon556678 <simon2233tw@gmail.com>
2025-05-10 21:22:18 +0800

    Complete the personal cover display for the first question

--- Hw3.c
+++ Hw3.c
@@ -1,15 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h> 
-#define PASSWORD "2025"
-#define AUTHOR "E1B52-YourName"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+#define AUTHOR "simon"  
+
+const int ROWS = 9;
+const int COLS = 9;
+char seats[ROWS][COLS];
 
 
-void clearScreen() {
-    system("cls||clear");
+void printWelcomeScreen() {
+    printf("********************************************************************\n");
+    printf("*                                                                  *\n");
+    printf("*                E1B52 Coding Welcome Screen                       *\n");
+    printf("*                                                                  *\n");
+    printf("*------------------------------------------------------------------*\n");
+    printf("*      AUTHOR: %s                 *\n", AUTHOR);
+    printf("*------------------------------------------------------------------*\n");
+    printf("*                                                                  *\n");
+    printf("*   EEEEE  1    BBBBB   55555  22222                               *\n");
+    printf("*   E      1    B    B  5          2                               *\n");
+    printf("*   EEEE   1    BBBBB   55555  22222                               *\n");
+    printf("*   E      1    B    B      5  2                                   *\n");
+    printf("*   EEEEE  1    BBBBB   55555  22222                               *\n");
+    printf("*                                                                  *\n");
+    printf("*------------------------------------------------------------------*\n");
+    printf("*Please enter the 4-digit password to continue (Password hint: 2025)*\n");
+    printf("*                                                                  *\n");
+    printf("********************************************************************\n");
 }
 
 
-void flushInput() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
