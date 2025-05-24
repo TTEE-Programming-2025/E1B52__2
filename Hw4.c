8bb8354b5b6e999196691970378ccbf13f33e72b
simon556678 <simon2233tw@gmail.com>
2025-05-24 16:38:59 +0800

    Define the student structure and complete Question 1: Personal profile (welcome screen).

--- Hw4.c
+++ Hw4.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define AUTHOR "E1B 52" 
-#define MAX_STUDENTS 10
+#define MAX_STUDENTS 100
 #define PASSWORD 2025
 
 typedef struct {
@@ -14,4 +14,24 @@ typedef struct {
     float english;
     float average;
 } Student;
-
+//personal profile
+void printWelcomeScreen() {
+    printf("********************************************************************\n");
+    printf("*                                                                  *\n");
+    printf("*                E1B52 Coding Welcome Screen                       *\n");
+    printf("*                                                                  *\n");
+    printf("*------------------------------------------------------------------*\n");
+    printf("*      AUTHOR: %s                 *\n", AUTHOR);
+    printf("*------------------------------------------------------------------*\n");
+    printf("*                                                                  *\n");
+    printf("*   EEEEE  1    BBBBB   55555  22222                               *\n");
+    printf("*   E      1    B    B  5          2                               *\n");
+    printf("*   EEEE   1    BBBBB   55555  22222                               *\n");
+    printf("*   E      1    B    B      5  2                                   *\n");
+    printf("*   EEEEE  1    BBBBB   55555  22222                               *\n");
+    printf("*                                                                  *\n");
+    printf("*------------------------------------------------------------------*\n");
+    printf("*Please enter the 4-digit password to continue (Password hint: 2025)*\n");
+    printf("*                                                                  *\n");
+    printf("********************************************************************\n");
+}
